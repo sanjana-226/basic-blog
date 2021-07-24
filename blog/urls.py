ba49bbc63e51bb4b5c42f97c8487c3999a8c8498
Sanjana <rpadaval@gmail.com>
2021-07-24 21:56:26 +0530

    added favicon

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('about',views.about,name='about' )
-    
+    path('about',views.about,name='about' ),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
 ]
